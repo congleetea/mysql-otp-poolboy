e0533c029b9f504df0544792ab38d294d7719321
congleetea <congleetea@gmail.com>
2016-07-07 21:49:03 +0800

    can use local or global pool

--- src/mysql_poolboy.erl
+++ src/mysql_poolboy.erl
@@ -57,34 +57,34 @@ child_spec({GlobalOrLocal, PoolName}, PoolArgs, MysqlArgs) ->
     poolboy:child_spec(PoolName, PoolArgs1, MysqlArgs).
 
 %% @doc Execute a mysql prepared statement with given params.
-execute({GlobalOrLocal, PoolName}, StatementRef, Params) ->
-    poolboy:transaction({GlobalOrLocal, PoolName}, fun(MysqlConn) ->
+execute(PoolName, StatementRef, Params) ->
+    poolboy:transaction(PoolName, fun(MysqlConn) ->
         mysql:execute(MysqlConn, StatementRef, Params)
     end).
 
 %% @doc Execute a mysql prepared statement with given params and timeout
-execute({GlobalOrLocal, PoolName}, StatementRef, Params, Timeout) ->
-    poolboy:transaction({GlobalOrLocal, PoolName}, fun(MysqlConn) ->
+execute(PoolName, StatementRef, Params, Timeout) ->
+    poolboy:transaction(PoolName, fun(MysqlConn) ->
         mysql:execute(MysqlConn, StatementRef, Params, Timeout)
     end).
 
 %% @doc Executes a query to a mysql connection in a given pool.
-query({GlobalOrLocal, PoolName}, Query) ->
-    poolboy:transaction({GlobalOrLocal, PoolName}, fun(MysqlConn) ->
+query(PoolName, Query) ->
+    poolboy:transaction(PoolName, fun(MysqlConn) ->
         mysql:query(MysqlConn, Query)
     end).
 
 %% @doc Executes a query to a mysql connection in a given pool with either
 %% list of query parameters or a timeout value.
-query({GlobalOrLocal, PoolName}, Query, ParamsOrTimeout) ->
-    poolboy:transaction({GlobalOrLocal, PoolName}, fun(MysqlConn) ->
+query(PoolName, Query, ParamsOrTimeout) ->
+    poolboy:transaction(PoolName, fun(MysqlConn) ->
         mysql:query(MysqlConn, Query, ParamsOrTimeout)
     end).
 
 %% @doc Executes a query to a mysql connection in a given pool with both
 %% a list of query parameters and a timeout value.
-query({GlobalOrLocal, PoolName}, Query, Params, Timeout) ->
-    poolboy:transaction({GlobalOrLocal, PoolName}, fun(MysqlConn) ->
+query(PoolName, Query, Params, Timeout) ->
+    poolboy:transaction(PoolName, fun(MysqlConn) ->
         mysql:query(MysqlConn, Query, Params, Timeout)
     end).
 
@@ -98,30 +98,30 @@ query({GlobalOrLocal, PoolName}, Query, Params, Timeout) ->
 %%  end.
 %%
 %% mysql_poolboy:with(mypool, fun (Conn) -> mysql:query(Conn, "SELECT...") end).
-with({GlobalOrLocal, PoolName}, Fun) when is_function(Fun, 1) ->
-    poolboy:transaction({GlobalOrLocal, PoolName}, Fun).
+with(PoolName, Fun) when is_function(Fun, 1) ->
+    poolboy:transaction(PoolName, Fun).
 
 %% @doc Executes a mysql transaction fun. The fun needs to take one argument
 %% which is the mysql connection.
-transaction({GlobalOrLocal, PoolName}, TransactionFun) when is_function(TransactionFun, 1) ->
-    poolboy:transaction({GlobalOrLocal, PoolName}, fun(MysqlConn) ->
+transaction(PoolName, TransactionFun) when is_function(TransactionFun, 1) ->
+    poolboy:transaction(PoolName, fun(MysqlConn) ->
         mysql:transaction(MysqlConn, TransactionFun, [MysqlConn], infinity)
     end).
 
 %% @doc Executes a transaction fun. Args list needs be the same length as
 %% TransactionFun arity - 1.
-transaction({GlobalOrLocal, PoolName}, TransactionFun, Args)
+transaction(PoolName, TransactionFun, Args)
     when is_function(TransactionFun, length(Args) + 1) ->
-    poolboy:transaction({GlobalOrLocal, PoolName}, fun(MysqlConn) ->
+    poolboy:transaction(PoolName, fun(MysqlConn) ->
         mysql:transaction(MysqlConn, TransactionFun, [MysqlConn | Args],
                           infinity)
     end).
 
 %% @doc Same as transaction/3 but with the number of retries the mysql
 %% transaction should try to execute.
-transaction({GlobalOrLocal, PoolName}, TransactionFun, Args, Retries)
+transaction(PoolName, TransactionFun, Args, Retries)
     when is_function(TransactionFun, length(Args) + 1) ->
-    poolboy:transaction({GlobalOrLocal, PoolName}, fun(MysqlConn) ->
+    poolboy:transaction(PoolName, fun(MysqlConn) ->
         mysql:transaction(MysqlConn, TransactionFun, [MysqlConn | Args],
                           Retries)
     end).
